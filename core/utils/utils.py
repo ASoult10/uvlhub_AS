413abf65fddf099e6ccd225863b9e0c162df772a
Izshin <ivanfernandezlimarquez757@gmail.com>
2025-11-10 20:45:03 +0100

    refactor(rec stystem): Linting in utils

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -1,11 +1,13 @@
 import random
 from datetime import datetime, timezone, timedelta
 
+
 # Generate a random datetime within a range
 def random_datetime(start, end):
     delta = end - start
     random_seconds = random.randint(0, int(delta.total_seconds()))
     return start + timedelta(seconds=random_seconds)
+
 
 # Example usage in your seeder:
 created_at = random_datetime(
