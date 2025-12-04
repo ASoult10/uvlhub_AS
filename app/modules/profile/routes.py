bde6ef5dabf131db88e4897158e49b7d7a10c129
macpercam15 <macpercam@alum.us.es>
2025-12-04 17:41:45 +0100

    feat: :fire: ver rol en perfil
    ahora se pueden ver los roles que tienes desde la info de tu perfil, y si te creas un perifl nuevo, te asigna automaticamente el rol 'user'
    
    re #15

--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -47,6 +47,13 @@ def my_profile():
     )
 
     total_datasets_count = db.session.query(DataSet).filter(DataSet.user_id == current_user.id).count()
+    roles_name = [r.name for r in current_user.roles] if current_user.roles else 'NNone'
+
+    print('====================================0')
+    print(roles_name)
+    print(roles_name)
+    print('====================================0')
+
 
     print(user_datasets_pagination.items)
 
@@ -57,6 +64,7 @@ def my_profile():
         datasets=user_datasets_pagination.items,
         pagination=user_datasets_pagination,
         total_datasets=total_datasets_count,
+        roles_name=roles_name,
     )
 
 
