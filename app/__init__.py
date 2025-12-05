007cd86291d365d3622ef41d51f3075985957010
ianamiranda <iana.mica04@gmail.com>
2025-12-05 16:39:41 +0100

    feat: :bug: Arreglo de configuracion SMTP
    Arreglo SMTP para que vuelva a funcionar el Recover my Password
    
    #31

--- app/__init__.py
+++ app/__init__.py
@@ -61,8 +61,13 @@ def create_app(config_name="development"):
     module_manager = ModuleManager(app)
     module_manager.register_modules()
 
-    # Initialize Flask-Mail
-    mail.init_app(app)
+    # Initialize Flask-Mail (simulado cambiar luego)
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USERNAME'] = 'astronomiahub@gmail.com'
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_DEFAULT_SENDER'] = 'astronomiahub@gmail.com'
 
     # Register login manager
     from flask_login import LoginManager
