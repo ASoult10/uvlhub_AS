281fdca4c43f7b2275c25674f558aa66db540ea1
Lauperfra <lauperfra@alum.us.es>
2025-12-11 11:54:10 +0100

    test: :test_tube: Tests de selenium Recover my password (casi terminados)
    Tests de selenium prácticamente terminados. Únicamente faltan algunas comprobaciones.
    
    Refs: #46

--- app/modules/auth/tests/test_selenium_recover_password.py
+++ app/modules/auth/tests/test_selenium_recover_password.py
@@ -1,53 +1,16 @@
+import pytest
 import time
-
-from selenium.common.exceptions import NoSuchElementException
+import json
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-
-from core.environment.host import get_host_for_selenium_testing
-from core.selenium.common import close_driver, initialize_driver
-
-
-def test_login_and_check_element():
-
-    driver = initialize_driver()
-
-    try:
-        host = get_host_for_selenium_testing()
-
-        # Open the login page
-        driver.get(f"{host}/login")
-
-        # Wait a little while to make sure the page has loaded completely
-        time.sleep(4)
-
-        # Find the username and password field and enter the values
-        email_field = driver.find_element(By.NAME, "email")
-        password_field = driver.find_element(By.NAME, "password")
-
-        email_field.send_keys("user1@example.com")
-        password_field.send_keys("1234")
-
-        # Send the form
-        password_field.send_keys(Keys.RETURN)
-
-        # Wait a little while to ensure that the action has been completed
-        time.sleep(4)
-
-        try:
-
-            driver.find_element(By.XPATH, "//h1[contains(@class, 'h2 mb-3') and contains(., 'Latest datasets')]")
-            print("Test passed!")
-
-        except NoSuchElementException:
-            raise AssertionError("Test failed!")
-
-    finally:
-
-        # Close the browser
-        close_driver(driver)
-
-# RECOVER MY PASSWORD TESTS
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
+from app.modules.auth.services import AuthenticationService
+from app import create_app,db
 
 FIREFOX_BIN = "/snap/firefox/current/usr/lib/firefox/firefox"
 
