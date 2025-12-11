ce55e6ece8243f2e92220bd624bb5eac3002df4a
Oscar Menendez Marquez <oscarmenendezmarquez@gmail.com>
2025-12-11 12:39:54 +0100

    feat: add import dataset functionality and update sidebar

--- app/modules/dataset/__init__.py
+++ app/modules/dataset/__init__.py
@@ -1,13 +1,13 @@
 from flask_restful import Api
-
 from app.modules.dataset.api import init_blueprint_api
 from core.blueprints.base_blueprint import BaseBlueprint
 
 dataset_bp = BaseBlueprint("dataset", __name__, template_folder="templates")
 
-
 api = Api(dataset_bp)
 init_blueprint_api(api)
 
-# Import comments routes to register them
-from app.modules.dataset import comments_routes  # noqa: F401, E402
+from app.modules.dataset.import_api import import_api
+dataset_bp.register_blueprint(import_api)
+
+from app.modules.dataset import comments_routes  # noqa
