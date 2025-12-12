0f9158571d5a5d65e75fb679d6175a641e55d2ac
Eloy Sancho Cebrero <eloy.sancho.cebrero@gmail.com>
2025-12-12 21:33:04 +0100

    test(explore): :white_check_mark: arreglo pequeño en el test de selenium
    Refs: #27

--- app/modules/explore/tests/test_selenium_explore.py
+++ app/modules/explore/tests/test_selenium_explore.py
@@ -10,15 +10,17 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestTestseleniumexplore():
+
+class TestSeleniumExplore():
   def setup_method(self, method):
     self.driver = webdriver.Firefox()
     self.vars = {}
-  
+
   def teardown_method(self, method):
     self.driver.quit()
-  
-  def test_testseleniumexplore(self):
+
+
+  def test_selenium_explore(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1854, 1048)
     self.driver.find_element(By.LINK_TEXT, "Explore").click()
@@ -50,4 +52,4 @@ class TestTestseleniumexplore():
     self.driver.find_element(By.ID, "select2-author-container").click()
     self.driver.find_element(By.ID, "select2-author-container").click()
     self.driver.find_element(By.ID, "select2-author-container").click()
-  
+
